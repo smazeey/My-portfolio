e381da10ac2868f76e4916f25746f9693d1c5661
smazeey <smazeey1@gmail.com>
2026-04-03 01:03:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+
+alert("Hello,Welcome!!")
+// add value
+
+ const form = document.getElementById("login");
+ form. addEventListener ("submit", function (event){ 
+  event.preventDefault ();
+  const email = document.getElementById("email").value.trim();
+  const name = document.getElementById("name").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  // logic
+
+  if ( password === "1234567" && email === "text@gmail.com"){
+
+    // call for windows redirection
+
+window.location.href = "index.html";
+alert("Welcome back  " + name + ("!."));
+
+// decision
+  }else{
+
+    // notification
+    alert("invalid login");
+
+  }
+  
+ })
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
